2e76f28c0de93a2b7fe3bf4d7f31e547983b00f7
Rémy Grünblatt <remy@grunblatt.org>
2021-07-01 14:18:17 +0200

    Cleanup of the warnings

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
-use std::env;
-use std::io::{self, Error, ErrorKind, Read, Write};
-use std::fs::{self, OpenOptions, File};
-use std::path::Path;
-use std::ffi::OsString;
-use libnar::{de::{EntryKind, Entries}, Archive};
-use xz::read::{XzEncoder, XzDecoder};
 use lazy_static::lazy_static;
+use libnar::{
+    de::{Entries, EntryKind},
+    Archive,
+};
 use regex::Regex;
 use reqwest;
+use std::env;
+use std::ffi::OsString;
+use std::fs::{self};
+use std::io::{self, Error, ErrorKind};
+use std::path::Path;
+use xz::read::XzDecoder;
 
 #[derive(Debug)]
 enum MyError {
@@ -16,7 +19,6 @@ enum MyError {
     E3(std::io::Error),
     E4(OsString),
 }
-
 
 impl From<reqwest::Error> for MyError {
     fn from(err: reqwest::Error) -> MyError {
@@ -36,18 +38,16 @@ impl From<OsString> for MyError {
     }
 }
 
-
 fn extract_hash(input: &str) -> Option<&str> {
     lazy_static! {
         static ref RE: Regex = Regex::new(r".*/?(?P<hash>[a-zA-Z0-9]{32})-?.*").unwrap();
     }
-    RE.captures(input).and_then(|cap| {
-        cap.name("hash").map(|hash| hash.as_str())
-    })
+    RE.captures(input)
+        .and_then(|cap| cap.name("hash").map(|hash| hash.as_str()))
 }
 
 fn create_dir(dst: &Path) -> io::Result<()> {
-    fs::create_dir(&dst).or_else(|err| {
+    fs::create_dir_all(&dst).or_else(|err| {
         if err.kind() == ErrorKind::AlreadyExists {
             let prev = fs::metadata(&dst);
             if prev.map(|m| m.is_dir()).unwrap_or(false) {
@@ -61,22 +61,25 @@ fn create_dir(dst: &Path) -> io::Result<()> {
     })
 }
 
-
 fn recurse<T: std::io::Read>(entries: Entries<T>, dst: &Path) -> Result<(), MyError> {
-    create_dir(dst);
+    create_dir(dst)?;
     for entry in entries {
         let mut entry = entry?;
         if entry.is_dir() {
             println!("Creating {:?}", dst.join(entry.name()));
-            create_dir(&dst.join(entry.name()));
-        } else if (entry.is_file() || entry.is_executable()) {
-                println!("Extracting file {:?} to {:?}", entry.name(), dst.join(entry.name()));
-                entry.unpack_in(dst)?
-        } else if (entry.is_symlink()) {
+            create_dir(&dst.join(entry.name()))?;
+        } else if entry.is_file() || entry.is_executable() {
+            println!(
+                "Extracting file {:?} to {:?}",
+                entry.name(),
+                dst.join(entry.name())
+            );
+            entry.unpack_in(dst)?
+        } else if entry.is_symlink() {
             println!("Symlink encountered. Recursing.");
             if let EntryKind::Symlink { target } = entry.kind {
                 println!("{:?}", target);
-                download(&target.into_os_string().into_string()?);
+                download(&target.into_os_string().into_string()?)?;
             }
         } else {
             println!("Unknown file type ! Skipping.");
@@ -85,50 +88,55 @@ fn recurse<T: std::io::Read>(entries: Entries<T>, dst: &Path) -> Result<(), MyEr
     Ok(())
 }
 
-fn getArchiveUrl(hash: &str) -> Result<String, MyError>{
-    let binaryCaches = ["https://cache.nixos.org/", "https://sisyphe.cachix.org/", "https://bincache.grunblatt.org/"];
-    for cache in binaryCaches.iter() {
+fn get_archive_url(hash: &str) -> Result<String, MyError> {
+    let binary_caches = [
+        "https://cache.nixos.org/",
+        "https://sisyphe.cachix.org/",
+        "https://bincache.grunblatt.org/",
+    ];
+    for cache in binary_caches.iter() {
         let url = format!("{}{}.narinfo", cache.to_owned(), hash);
         let resp = reqwest::blocking::get(url)?;
         match resp.status() {
             reqwest::StatusCode::OK => {
-                return Ok(cache.to_string() + &resp.text()?.split("\n").collect::<Vec<&str>>()[1].to_string().split(": ").collect::<Vec<&str>>()[1].to_string());
-            },
+                return Ok(cache.to_string()
+                    + &resp.text()?.split("\n").collect::<Vec<&str>>()[1]
+                        .to_string()
+                        .split(": ")
+                        .collect::<Vec<&str>>()[1]
+                        .to_string());
+            }
             _ => {}
         }
     }
-    return Err(MyError::E1())
+    return Err(MyError::E1());
 }
 
 fn download(path_or_hash: &str) -> Result<(), MyError> {
     let hash = extract_hash(path_or_hash).unwrap();
     /* Search hash narinfo in binary caches */
-    let url = getArchiveUrl(hash)?;
+    let url = get_archive_url(hash)?;
     println!("{}", url);
     /* Get the archive if it has been found */
     let resp = reqwest::blocking::get(url)?;
     match resp.status() {
         reqwest::StatusCode::OK => {
             /* Decompress the archive using xz */
-            let mut decompressor = XzDecoder::new(resp);
+            let decompressor = XzDecoder::new(resp);
             let mut nar = Archive::new(decompressor);
             let entries = nar.entries().unwrap();
             let dirname = format!("./result/{}", hash);
             let path = Path::new(&dirname);
-            recurse(entries, path);/* 
-            for entry in entries {
-                let entry = entry.unwrap();
-                println!("{:?}", entry);
-            }*/
-            return Ok(())
-        },
-        _ => ()
-    }  
-    Ok(())
+            recurse(entries, path)
+        }
+        _ => Err(MyError::E1()),
+    }
 }
 
 fn main() -> Result<(), MyError> {
-    let hash = env::args().nth(1).expect("Expected path name or hash of a nar archive");
-    download(&hash);
+    let hash = env::args()
+        .nth(1)
+        .expect("Expected path name or hash of a nar archive");
+    download(&hash)?;
     Ok(())
 }
